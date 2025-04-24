8d886a13896c19fd2c98a5be257f267caa41b49b
ahballif <addisonballif@gmail.com>
2025-04-23 19:52:54 -0600

    added the rendering function to camera

--- entities/Camera.py
+++ entities/Camera.py
@@ -1,13 +1,17 @@
 from numpy import array, sin, cos, pi
 
 class Camera:
-    def __init__(self, x = 0, y = 0, z = 0, theta = pi/2, phi=0, ):
+    def __init__(self, x = 0, y = 0, z = 0, theta = pi/2, phi=0, nx=400, ny=400):
     
         self.x = x
         self.y = y
         self.z = z
         self.theta = theta
         self.phi = phi
+
+        # camera canvas coordinates. 
+        self.nx = nx 
+        self.ny = ny
 
     def getCombinedMatrix(self):
         translationMatrix = array([[1, 0, 0, -self.x],
@@ -32,7 +36,22 @@ class Camera:
                                 [0, 0, -1*(far + near)/(far - near), -2*far*near/(far-near)],
                                 [0, 0, -1, 0]])
         
-        return projectionMatrix @ cameraMatrix
-    
-    def worldToScreen(inputVec, step1Mat, step2Mat):
-        pass
+        # now make the viewport matrix. 
+        viewportMatrix = array([[self.nx/2, 0, 0, (self.nx-1)/2], 
+                                [0, self.ny/2, 0, (self.ny-1)/2], 
+                                [0, 0, 0.5, 0.5]])
+
+        return projectionMatrix @ cameraMatrix, viewportMatrix
+
+    def getScreenCoords(self, inputCoordinates):
+        # take in a list of input vectors and return the corresponding screen vectors 
+        newVectors = inputCoordinates
+        camMat, vpMat = self.getCombinedMatrix()
+        for i in range(len(inputCoordinates)):
+            # first project the coordinate onto the screen
+            step1 = camMat @ inputCoordinates[i] 
+            # now scale it so that the first entry is one
+            step2 = step1/step1[3]
+            # now apply the viewport matrix 
+            step3 = vpMat @ step2
+            newVectors[i] = step3
