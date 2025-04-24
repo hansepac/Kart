75351e34e3b68b45eb28d64d8fb96e4850501ff8
Hans Capener <131813389+hansepac@users.noreply.github.com>
2025-04-23 22:47:35 -0600

    Added Driver inputs

--- entities/Driver.py
+++ entities/Driver.py
@@ -6,16 +6,27 @@ class Driver:
         self.pos = array([x, y, z])
         self.vel = array([0, 0, 0])
         self.acc = array([0, 0, 0])
+        self.turn_angle = 0
+        self.turn_speed = 0.01
 
         self.rank = 0
+
+        self.inputs = {
+            "forward": False,
+            "backward": False,
+            "left": False,
+            "right": False,
+            "drift": False,
+            "use_item": False
+        }
 
     # put position/velocity stuff here 
     
     # put sprite stuff here. 
 
-    def updatePosition():
-        # this is where the physics happens
-        pass 
+    def updatePosition(self):
+        if self.inputs["forward"]:
+            self.vel[0] += 1
 
-    def returnCurrentSprite():
+    def returnCurrentSprite(self):
         pass
