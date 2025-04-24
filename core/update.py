78f9e4f2eba933931fb36a0dd337970df92c54a6
Hans Capener <131813389+hansepac@users.noreply.github.com>
2025-04-23 20:18:14 -0600

    Added keyboard update to update func

--- core/update.py
+++ core/update.py
@@ -1,8 +1,10 @@
-from __init__ import gameState
+from __init__ import gameState, keyboard
 import core.title as title
 import core.game as game
 
 def update():
+    # Get all the latest key presses
+    keyboard.update()
     # Run updates based on gameState
     if gameState == gameState.TITLE:
         title.update()
