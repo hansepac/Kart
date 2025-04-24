42e1f3c74eb5c30f21441aaf5dba13f828199b9e
Hans Capener <131813389+hansepac@users.noreply.github.com>
2025-04-23 20:18:48 -0600

    Update Dots

--- entities/Dots.py
+++ entities/Dots.py
@@ -1,12 +1,15 @@
 from __init__ import pg
 from random import randint
+from numpy import array
 
 class Dots():
     def __init__(self, x: int, y: int, z: int, color = (randint(100, 255), randint(100, 255), randint(100, 255))):
-        self.x = x
-        self.y = y
+        self.pos = array([x, y, z])
         self.color = color
         self.size = 5
 
-    def draw(self, screen):
-        pg.draw.circle(screen, self.color, (self.x, self.y), self.size)
+    def get_pos(self):
+        return array([*self.pos, 1])
+
+    def draw(self, screen, win_x, win_y):
+        pg.draw.circle(screen, self.color, (win_x, win_y), self.size)
