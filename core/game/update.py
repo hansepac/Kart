904908856663f14be568a27f3daef8e63ea291f3
Hans Capener <131813389+hansepac@users.noreply.github.com>
2025-05-05 00:33:03 -0600

    Added Local Functionality

--- core/game/update.py
+++ core/game/update.py
@@ -1,24 +1,26 @@
 from utils.cores import Core, GameCore
 
 def update(c: Core, gc: GameCore):
-    local_game_data = gc.mapMaster.get_game_data()
-    server_data = gc.client.send_to_server(local_game_data)
-    
-    server_game_data = None
-    if server_data:
-        for response in server_data:
-            if response['msg_type'] == "live_data":
-                server_game_data = response["dat"]
-            elif response['msg_type'] == "game_setup":
-                server_game_data = response["dat"]
-            elif response['msg_type'] == "new_client":
-                print(f"NEW: {response['dat']}")
-            elif response['msg_type'] == "drop_client":
-                print(f"DROP: {response['dat']}")
-            
+    if c.onlineState != c.onlineState.LOCAL:
+        # Get game data from server
+        local_game_data = gc.mapMaster.get_game_data()
+        server_data = gc.client.send_to_server(local_game_data)
+        
+        server_game_data = None
+        if server_data:
+            for response in server_data:
+                if response['msg_type'] == "live_data":
+                    server_game_data = response["dat"]
+                elif response['msg_type'] == "game_setup":
+                    server_game_data = response["dat"]
+                elif response['msg_type'] == "new_client":
+                    print(f"NEW: {response['dat']}")
+                elif response['msg_type'] == "drop_client":
+                    print(f"DROP: {response['dat']}")
+                
 
-    if server_game_data:
-        gc.mapMaster.update_from_server(server_data)
+        if server_game_data:
+            gc.mapMaster.update_from_server(server_data)
     gc.mapMaster.update(c)
 
     return c
