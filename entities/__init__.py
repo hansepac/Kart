793437c53bc88a5f2b890bff10b438ae03da2535
ahballif <addisonballif@gmail.com>
2025-05-05 21:57:39 -0600

    fixed import bug

--- entities/__init__.py
+++ entities/__init__.py
@@ -3,5 +3,5 @@ from .Dots import Dots
 from .Track import Track
 from .MapMaster import MapMaster
 from .LocalPlayer import LocalPlayer
-from .Terrain import Terrain, TerrainGrid, TerrainDynamic, TerrainDynamicCoordinator
+from .Terrain import TerrainDynamic, TerrainDynamicCoordinator
 from .Renderable import Renderable, TerrainTriangle, DriverSprite
