f664437d351315cc2618210cb6d19b04d08d25b6
ahballif <addisonballif@gmail.com>
2025-05-07 19:33:48 -0600

    Update LocalPlayer.py

--- entities/LocalPlayer.py
+++ entities/LocalPlayer.py
@@ -133,19 +133,6 @@ class LocalPlayer(Driver):
             self.camera.y = (1-camera_theta_move_rate)*self.camera.y + camera_theta_move_rate*(ground_height + self.camera_height)
             self.camera.z = cam_pos[2]
 
-    def updateCameraPositonOld(self):
-        if self.gameDebugState != self.gameDebugState.FLY_DEBUG:
-            #               TODO: Adjust camera position factoring in the theta angle of ground
-            self.camera.theta = -np.atan2(self.camera_height, self.camera_distance)
-            self.camera.phi = np.atan2(self.direction_unitvec[2], self.direction_unitvec[0]) + np.pi/2
-            
-            horizontal_unitvec = self.direction_unitvec/np.linalg.norm(self.direction_unitvec)
-            horizontal_unitvec[1] = 0
-            cam_pos = self.pos - self.camera_distance*horizontal_unitvec + np.array([0, 1, 0])*self.camera_height
-            self.camera.x = cam_pos[0]
-            self.camera.y = cam_pos[1]
-            self.camera.z = cam_pos[2]
-
 
     def control(self, events):
         inputs = self.controller.get_input(events)
